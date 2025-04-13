0e446fe2933450a0f6316ccc6ff1cfbffb8ef7de
Jojohere05 <2005jhotikaraja@gmail.com>
2025-04-14 01:31:14 +0530

    init scanner

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
-    
+    private static ArrayList<Student> students = new ArrayList<>();
+    private static Scanner sc = new Scanner(System.in);
+
 }
