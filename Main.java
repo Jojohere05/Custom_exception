4158cf0af9a17f83341b376f5547f36ba804f985
Jojohere05 <2005jhotikaraja@gmail.com>
2025-04-14 01:38:38 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -14,17 +14,11 @@ public class Main {
             System.out.println("6. Exit");
             System.out.print("Choose an option: ");
             int choice = sc.nextInt();
-            sc.nextLine(); 
-            try {
-                switch (choice) {
-                    case 1: addStudent(); break;
-                    case 2: displayStudents(); break;
-                    case 3: searchStudent(); break;
-                    case 4: updateStudent(); break;
-                    case 5: deleteStudent(); break;
-                    case 6: System.exit(0);
-                    default: System.out.println("Invalid option. Try again.");
-                }
-            } 
+            sc.nextLine();
+           t
+        }
     }
+
 }
+
+
