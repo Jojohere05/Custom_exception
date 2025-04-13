038e7e76ee6f2493e376d52e706b48db1dbc53a2
Jojohere05 <2005jhotikaraja@gmail.com>
2025-04-14 01:33:28 +0530

    options available to users

--- Main.java
+++ Main.java
@@ -4,5 +4,16 @@ import java.util.Scanner;
 public class Main {
     private static ArrayList<Student> students = new ArrayList<>();
     private static Scanner sc = new Scanner(System.in);
-
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("1. Add Student");
+            System.out.println("2. Display Students");
+            System.out.println("3. Search Student");
+            System.out.println("4. Update Student");
+            System.out.println("5. Delete Student");
+            System.out.println("6. Exit");
+            System.out.print("Choose an option: ");
+            int choice = sc.nextInt();
+            sc.nextLine(); 
+    }
 }
